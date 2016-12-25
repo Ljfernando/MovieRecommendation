df899d91b38c011c8fa5c589c6481c14aec0643a
Ljfernando <Ljfernando@dons.usfca.edu>
2016-12-25 01:46:48 -0800

    Added information about similarity metric computations to readme files. Added method descriptions to python program

--- MovieRecommendation.py
+++ MovieRecommendation.py
@@ -1,8 +1,15 @@
 __author__ = 'lancefernando'
+"""This Movie Recommendation program utilizes collaborative filtering to recommend movies the user may enjoy. 
+	Data used is original data from MovieLens. It will ask a user to rate 50 random movies on a scale 
+	from 0 to 5 where the 0 value means that the user has not seen the movie. The ratings are stored as a vector. 
+	This vector is then compared to other user ratings using two different similarity measurements:
+	Cosine similarity and Jaccard similarity. The 5 most similar user vectors are then utilized to recommend 
+	up to five different movies which are outputted to the console and as a text file. Movies that are recommended 
+	must have be rated a 5 by the recorded similar user. """
 import random
-## Function reads movie titles from a given movie file and adds each movie title
-## to a list 
 
+"""Function reads movie titles from a given movie file and adds each movie title
+	to a list. List of movie titles is then returned."""
 def readMovies(filename):
 
     moviefile = open(filename, 'r')
@@ -15,15 +22,15 @@ def readMovies(filename):
     moviefile.close()
     return movieList
 
-## Function uses given list and asks user to rate each movie in list and stores each
-## rating into given filename
+"""Function uses given list and asks user to rate each movie in list. Each rating is then written
+	to the given filename."""
 def profilemaker(filename, movieList):
     ratingArray = ['0'] * len(movieList) #number of movies
-    randomMovies = random.sample(range(1,len(movieList)), 50)
+    randomMovies = random.sample(range(len(movieList)), 50) #Grabbing 50 random movies from list
     ratingString = ''
-    print("Please create your profile by rating the following movies from 0 to 5 (0 if not watched): ")
+    print("Please create your profile by rating the following movies from 0 to 5 (1 being horrible, 5 being amazing and 0 if not watched): ")
+
     for movie in randomMovies:
-        #iterates over first 50 movies
         print(movieList[movie])
         rating = raw_input("Enter your rating: ")
 
@@ -36,27 +43,28 @@ def profilemaker(filename, movieList):
 
     for each in ratingArray:
         ratingString += each + ' '
+
     f = open(filename, 'w')
     f.write(ratingString)
     f.close()
 
-##    Function will read given rating profile and returns list
-##    of ratings
+"""Function will read given rating profile and returns list of ratings"""
 def readProfile(filename):
 
     f = open(filename, 'r')
     lines = f.readlines()
     ratings = lines[0]
     ratings = ratings.strip(' ')
-    #removing any extra white space on ends
     ratings = ratings.split(' ')
+
     for i in range(0, len(ratings)):
         ratings[i] = int(ratings[i])
     f.close()
     return ratings
 
-##     """Function creates a dictionary using given file. Each key is the user
-##    and the value is the user's rating and movie rated"""   
+"""Function creates a dictionary using given file. Each key is the user
+	and the value is a list of with the movies rated at index 0 and a list of ratings at index 1.
+	This dictionary is then returned.""" 
 def movieRatings(filename):
 
     ratingDict ={}
@@ -83,13 +91,17 @@ def movieRatings(filename):
             #If user is not in dictionary yet, this will add user
             #and the user's rating
     ratings.close()
+
     return ratingDict
 
+"""Takes a vector of ratings (user1) and a list of lists with movie IDs and their respective ratings
+	as user2 and returns the item-item cosine similarity between movies rated."""
 def computeCosSim(user1, user2):
 	dotProd = 0
 	user2Prod = 0
 	user1Prod = 0
 
+	# Computing the dot product between user1 and user2 ratings
 	for i in range(len(user2)):
 
 		movieID = user2[i][0]
@@ -97,33 +109,39 @@ def computeCosSim(user1, user2):
 
 		dotProd += rating * user1[movieID - 1]
 
-		user2Prod += rating **2
+		user2Prod += rating **2 #computing the user2 dot product of itself
 
+	# Computing the user1 dot product of itself
 	for i in range(len(user1)):
 
 		user1Prod += user1[i] **2
 
 	return float(dotProd)/(float(user1Prod)*float(user2Prod))
 
+"""Takes a vector of ratings (user1) and a list of lists with movie IDs and their respective ratings
+	as user2 returns the item-item jaccard similarity between movies rated."""
 def computeJacSim(user1, user2):
 
 	intersection = 0
-	union = len(user1)
+	union = len(user1) # this is true since len(user1) is the whole sample size of the movie list
 
 	for i in range(len(user2)):
 
 		movieID = user2[i][0]
 		rating = user2[i][1]
+
+		# Values count as intersection when both users rate the same movie as a 3 or greater
 		if user1[movieID - 1] >= 3 and rating >=3:
 			intersection += 1
+
 	return float(intersection)/float(union)
 
-
-def computeRecommendation(ratingFile, moviesFile, userFile, recCosFile, recJacFile):
-    """Function will call all previous functions and determine movies
-    that the user should watch by comparing their ratings to the ratings of
-    other users. Movies that are recommended must have a rating of 5 by the calculated
+"""Calls all previous functions and determine movies that the user should watch 
+	by comparing their ratings to the ratings of
+    other users. Movies that are recommended must have a rating of 5 b y the calculated
     similar users."""
+def computeRecommendation(ratingFile, moviesFile, userFile, recCosFile, recJacFile):
+
     ratingDict = movieRatings(ratingFile)
     movieList = readMovies(moviesFile)
     profilemaker(userFile, movieList)
@@ -134,11 +152,13 @@ def computeRecommendation(ratingFile, moviesFile, userFile, recCosFile, recJacFi
 
     top5ListCos = []
     top5ListJac = []
-    #creating empty list for top 5 compatible users
+    #creating empty lists for top 5 compatible users
+
     recommendListCos = []
     recommendListJac = []
-    #creating empty recommended movie list for
+    #creating empty recommended movie lists
 
+    #computing cosine and jaccard similarity values between the client and other users' movie ratings
     for user in ratingDict.keys():
         otherUsers = ratingDict[user]
 
@@ -149,12 +169,14 @@ def computeRecommendation(ratingFile, moviesFile, userFile, recCosFile, recJacFi
         top5ListCos.append([cosSim, user])
         top5ListJac.append([jacSim, user])
 
+    #sorting list in descending order to grab the users that are most similar to the client
     top5ListCos = sorted(top5ListCos, reverse = True)
     top5ListJac = sorted(top5ListJac, reverse = True)
 
     top5ListCos = top5ListCos[0:5]
     top5ListJac = top5ListJac[0:5]
 
+    #Creating a list of recommended movies from the most similar users based on cosine similarity
     for sim in range(0, 5):
         key = top5ListCos[sim][1]
         movies = ratingDict[key]
@@ -171,6 +193,8 @@ def computeRecommendation(ratingFile, moviesFile, userFile, recCosFile, recJacFi
                         counter += 1
             else:
                 break
+
+    #Creating a list of recommended movies from the most similar users based on jaccard similarity
     for sim in range(0, 5):
         key = top5ListJac[sim][1]
         movies = ratingDict[key]
@@ -187,6 +211,8 @@ def computeRecommendation(ratingFile, moviesFile, userFile, recCosFile, recJacFi
                         counter += 1
             else:
                 break
+
+    #writing recommended lists to given files            
     for i in range(0, len(recommendListCos)):
         cosFile.write(recommendListCos[i] + '\n')
     cosFile.close()
@@ -195,48 +221,51 @@ def computeRecommendation(ratingFile, moviesFile, userFile, recCosFile, recJacFi
         jacFile.write(recommendListJac[i] + '\n')
     jacFile.close()
 
-
+"""Asks client which similarity metric they prefer to have movies recommended by.
+	returns 0 if client chooses cosine similarity or 1 for jaccard similarity."""
 def getSimilarityType():
 
-	print("We have computed recommendations based on two different similarity metrics. ")
-	similarity = raw_input("Enter 0 for Cosine Similarity recommendations or 1 for Jaccard Similarity recommendations: ")
+    print("Based on your ratings and others that have rated similarly we have created two lists in no particular order of movies for you to watch.")
+    print("The two lists of recommendations were computed based on two different similarity metrics. \n")
+    similarity = raw_input("Enter 0 to view movies based on the Cosine-Similarity metric or 1 for Jaccard-Similarity recommendations: ")
 
-	while(not(similarity.isdigit()) or int(similarity) not in range(0,2)):
-		similarity = raw_input("Please enter a valid entry [0 or 1]: ")
+    while(not(similarity.isdigit()) or int(similarity) not in range(0,2)):
+    	similarity = raw_input("Please enter a valid entry [0 or 1]: ")
 
 	return int(similarity)
 
-
+"""Prints movies line by line from a given file to the console."""
 def printMoviesFromFile(filename):
 	file = open(filename, 'r')
 	for line in file:
 		print(line)
 	file.close()
 
+"""Calls printMoviesFromFile() function to print a list of movies based on the similarity metric
+	the client chooses."""
 def printRecommendedMovies(file1, file2, simType):
 
 	if simType == 0:
 		print("Here are your recommendations based on Cosine similarity. \n")
 		printMoviesFromFile(file1)
 
-		response = raw_input("Would you like to view recommended movies based on Jaccard similarity? Yes or No: ")
+		response = raw_input("Would you also like to view recommended movies based on Jaccard similarity? Yes or No: ")
 		if response.lower() == 'yes':
 			printMoviesFromFile(file2)
 	else:
 		print("Here are your recommendations based on Jaccard similarity. \n")
 		printMoviesFromFile(file2)
 
-		response = raw_input("Would you like to view recommended movies based on Cosine similarity? Yes or No: ")
+		response = raw_input("Would you also like to view recommended movies based on Cosine similarity? Yes or No: ")
 		if response.lower() == 'yes':
 			printMoviesFromFile(file1)
 
+	print("Thank you!")
 
 def main():
 
     computeRecommendation('movieRatings.txt', 'movies.txt', 'profile.txt', 'cosRecommendedMovies.txt', 'jacRecommendedMovies.txt')
-    
-    print('Based on your ratings and others that have rated similarly we have created a list in no particular order of movies for you to watch. \n')
-    
+        
     simType = getSimilarityType()
 
     printRecommendedMovies('cosRecommendedMovies.txt', 'jacRecommendedMovies.txt', simType)
--- README.txt
+++ README.txt
@@ -1,69 +1,96 @@
-This Movie Recommendation program utilizes collaborative filtering to recommend movies the user may enjoy. Data used is original data from MovieLens(link below). It will ask a user to rate 50 random movies on a scale from 0 to 5 where the 0 value means that the user has not seen the movie. The ratings are stored as a vector. This vector is then compared to other user’s ratings using two different similarity measurements: Cosine similarity and Jaccard similarity. The 5 most similar user vectors are then utilized to recommend up to five different movies which are outputted to the console and as a text file. Movies that are recommended must have be rated a 5 by the recorded similar user. 
+This Movie Recommendation program utilizes collaborative filtering to recommend movies the user may enjoy. Data used is original data from MovieLens(link below). It will ask a user to rate 50 random movies on a scale from 0 to 5 where the 0 value means that the user has not seen the movie. The ratings are stored as a vector. This vector is then compared to other user’s ratings using two different similarity measurements: Cosine similarity and Jaccard similarity. The 5 most similar user vectors are then utilized to recommend up to five different movies which are outputted to the console and as a text file. Movies that are recommended must have been rated a 5 by the recorded similar user. Two lists of recommended movies are created based on the two different similarity metrics and both lists can then be viewed. 
 
-In order to run this program, download all necessary txt files and the MovieRecommendation.py file. Simply run the python file using python MovieRecommendation.py on your command line. You will then be asked to rate 50 random movies. Your rating profile will be saved in your current directory as ‘profile.txt’. The list of recommended movies will be saved as ‘cosRecommendedMovies.txt’ for movies recommended by Cosine similarity metric and as ‘jacRecommendedMovies.txt’ for movies recommended by Jaccard similarity metric.
+In order to run this program, download all necessary txt files(‘movies.txt’ & movieRatings.txt’) and the MovieRecommendation.py file. Simply run the python file using command ’python MovieRecommendation.py’ on command line or program of choice. You will then be asked to rate 50 random movies. Your rating profile will be saved in your current directory as ‘profile.txt’. The list of recommended movies will be saved as ‘cosRecommendedMovies.txt’ for movies recommended by the Cosine similarity metric and as ‘jacRecommendedMovies.txt’ for movies recommended by the Jaccard similarity metric.
+
+
+————————————
+Similarity Metrics
+————————————
+
+Cosine Similarity:
+This approach defines the similarity between two users x and y as:
+sim(x,y) = (x . y)/ ||x|| * ||y|| where x and y are vectors and (.) represents the dot product.
+
+Dot product of two vectors is the result of their linear combination.
+ex. a <- [1, 0, 3]; b <- [2, 1, 1]
+a.b = 1*2 + 0*1 + 3*1 = 2 + 0 + 3 = 5
+
+||x|| represents the vector x ‘dotted’ with itself.
+
+All similarity values will be bounded from [0:1] where larger values equate to a stronger similarity.
+
+
+Jaccard Similarity: 
+This approach defines the similarity between users x and y as:
+sim(x,y) = (x ∩ y)/ (x ∪ y) where x and y are vectors.
+∩ is the intersection between two sets. For this program, the vectors x and y intersect where the values as the same index are ≥ 3. This allows for rated movies to only count if they are more preferable than not (given a rating of 3 or greater). 
+
+∪ is the union between the two sets. This value is the number of movies that could have possibly been rated. All movies are accounted for, regardless of whether or not it was watched and rated.
+
+All similarity values will be bounded from [0:1] where larger values equate to a stronger similarity.
+
 
 ————————————
 Files
 ————————————
-
-~~~~~~~~~~~~~
 Python File:
+
 MovieRecommendation.py
-Methods:
-readMovies(filename):
--Takes in the text file of movies to create an array of all the movies. 
--The index of the movie is based on the movie ID - 1 since indexing begins at 0.
--Returns array of movies
+	Methods:
+	readMovies(filename):
+	-Takes in the text file of movies to create an array of all the movies. 
+	-The index of the movie is based on the movie ID - 1 since indexing begins at 0.
+	-Returns array of movies
 
-profileMaker(filename,movieList):
--Creates a profile(vector) of movie ratings.
--Movies rated are randomly selected from moveList array.
--This vector is written to a text file designated as filename.
+	profileMaker(filename,movieList):
+	-Creates a profile(vector) of movie ratings.
+	-Movies rated are randomly selected from moveList array.
+	-This vector is written to a text file designated as filename.
 
-readProfile(filename):
--Takes in a given file of a single users ratings.
--Returns an array of the users ratings
+	readProfile(filename):
+	-Takes in a given file of a single users ratings.
+	-Returns an array of the users ratings
 
-movieRatings(filename):
--Takes in a given file of movie ratings.
--The file is split to read line by line
--Each line is added to a dictionary where the key is the user number
--The value is a 2D list where the first column is the movie ID and the second is the user’s rating of that movie
--Returns the dictionary
+	movieRatings(filename):
+	-Takes in a given file of movie ratings.
+	-The file is split to read line by line
+	-Each line is added to a dictionary where the key is the user number
+	-The value is a 2D list where the first column is the movie ID and the second is the user’s rating of that movie
+	-Returns the dictionary
 
-computeCosSim(user1, user2):
--Computes the cosine similarity value [0:1] 
--user1 is a vector of ratings from the user running program
--user2 is a list of lists of a single users movieID and rating
--Returns cosine similarity value
+	computeCosSim(user1, user2):
+	-Computes the cosine similarity value [0:1] 
+	-user1 is a vector of ratings from the user running program
+	-user2 is a list of lists of a single users movieID and rating
+	-Returns cosine similarity value
 
-computeJacSim(user1, user2):
--Computes Jaccard similarity value [0:1]
--similar parameters as computeCosSim() method
--Returns Jaccard similarity value
+	computeJacSim(user1, user2):
+	-Computes Jaccard similarity value [0:1]
+	-similar parameters as computeCosSim() method
+	-Returns Jaccard similarity value
 
-computeRecommendation(ratingFile, moviesFile, userFile, recCosFile, recJacFile):
--Utilizes the dictionary of ratings and computes the cosine and jaccard similarity of each user to the original user
--Two lists of users based on their cosine similarity metric and jacquard similarity metric are created and sorted from greatest to least(most similar to least similar)
--The 5 most similar users are then analyzed to find at most five movies to recommend to the original user that they have not yet seen.
--Movies that are rated a 5 by the similar users are added to a recommendation list.
--Two separate lists of recommended movies based on their respective similarity metric are then written to the given text files.
+	computeRecommendation(ratingFile, moviesFile, userFile, recCosFile, recJacFile):
+	-Utilizes the dictionary of ratings and computes the cosine and jaccard similarity of each user to the original user
+	-Two lists of users based on their cosine similarity metric and jacquard similarity metric are created and sorted from 	greatest to least(most similar to least similar)
+	-The 5 most similar users are then analyzed to find at most five movies to recommend to the original user that they have not yet seen.
+	-Movies that are rated a 5 by the similar users are added to a recommendation list.
+	-Two separate lists of recommended movies based on their respective similarity metric are then written to the given text files.
 
-getSimilarityType():
--Asks user for which similarity metric they prefer to use
--Returns 0 for Cosine similarity and 1 for Jaccard similarity
+	getSimilarityType():
+	-Asks user for which similarity metric they prefer to use
+	-Returns 0 for Cosine similarity and 1 for Jaccard similarity
 
-printMoviesFromFile(filename):
--Reads in given file of movies
--Prints each movie line by line to console
+	printMoviesFromFile(filename):
+	-Reads in given file of movies
+	-Prints each movie line by line to console
 
-printRecommendedMovies(file1, file2, simType):
--Calls printMoviesFromFile() depending on simType value
--Then proceeds to ask if user would like to view recommended movies based on other metric
+	printRecommendedMovies(file1, file2, simType):
+	-Calls printMoviesFromFile() depending on simType value
+	-Then proceeds to ask if user would like to view recommended movies based on other metric
 
-main():
--Calls computeRecommendation() method with given parameters for file names
--Then prints recommended movies to console based on the users choice of similarity type.
+	main():
+	-Calls computeRecommendation() method with given parameters for file names
+	-Then prints recommended movies to console based on the users choice of similarity type.
 
 Data Files:
 
